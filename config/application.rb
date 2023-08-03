dbc92a0de41f13b7b54fff49110218861976aefd
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-03 23:05:07 +0900

    cors設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
