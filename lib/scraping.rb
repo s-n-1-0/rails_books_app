76df26da8b9da62f8e3a85b177327b1bf22f8f68
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-03 18:37:28 +0900

    スクレイピング処理の追加

---
+++ lib/scraping.rb
@@ -0,0 +1,38 @@
+class AmazonScraping
+    def searchPaperAsin(kindle_asin)
+      paper_asin = nil
+      agent = Mechanize.new
+      page = agent.get("https://www.amazon.co.jp/dp/"+kindle_asin)
+      ellist = page.search("#tmmSwatches")
+      ak = nil
+      if ellist.length > 0 then
+         alist = ellist[0].search("a")
+         asin_list = alist.map{|el|
+            ret_id = nil
+            url = el.attributes["href"].value
+            split_url = url.split("/")
+            dp_index = split_url.index("dp")
+            if !dp_index.nil? && (dp_index + 1) < split_url.length then
+               ret_id = split_url[dp_index + 1]
+            end
+            ret_id
+            }.select{|id| !id.nil?}
+         if asin_list.length > 0 then
+            paper_asin = asin_list[0]
+         end
+
+         if kindle_asin == paper_asin then
+            paper_asin = nil
+         end
+         #ソースのasinを変換したらB0から始まる場合、
+         #kindle_asinがB0から始まらないなら -> 紙の本
+         #始まるなら -> 変換できていない(変換先がない)
+         if !paper_asin.nil?
+            if paper_asin.start_with?("B0") then
+               paper_asin = kindle_asin
+            end
+         end
+      end
+      return paper_asin
+    end
+ end
---
+++ test/libs/scraping_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+require Rails.root.join("lib/scraping.rb") 
+
+class KindleTest < ActiveSupport::TestCase
+   test "電子->紙ASIN変換" do
+      scrapingObj = AmazonScraping.new()
+      id = scrapingObj.searchPaperAsin("B0C6993WBQ")
+      assert id == "4088836448"
+   end
+
+   test "紙->紙ASIN変換" do
+      scrapingObj = AmazonScraping.new()
+      id = scrapingObj.searchPaperAsin("4088836448")
+      assert id == "4088836448"
+   end
+
+   test "紙ASINが存在しないケース(電子書籍のみ)" do
+      scrapingObj = AmazonScraping.new()
+      id = scrapingObj.searchPaperAsin("B0BRMKV148")
+      assert id == nil
+   end
+   
+   test "そもそも書籍ASINではないケース(お茶のASIN)" do
+      scrapingObj = AmazonScraping.new()
+      id = scrapingObj.searchPaperAsin("B00IRVRZWA")
+      assert id == nil
+      end
+end
