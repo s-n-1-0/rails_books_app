876ffcfc6b1846b1533d9251074b65d2dc98f742
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-08-03 19:15:43 +0900

    Kindleモデルのバリエーション等設定

--- app/models/kindle.rb
+++ app/models/kindle.rb
@@ -1,2 +1,4 @@
 class Kindle < ApplicationRecord
+    validates :kindle_asin, length: 10..10, allow_blank: true
+    validates :paper_asin, length: 10..10, allow_blank: true
 end
--- test/models/kindle_test.rb
+++ test/models/kindle_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class KindleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @kindle = kindles(:onepiece106)
+  end
+  test "should be valid" do
+    assert @kindle.valid?
+  end
 end
